56249d379d9b9a8514ab84bba5590a89cae809e3
Julie  McManus <julieannesays@gmail.com>
2020-01-22 00:34:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,12 @@
-require 'pry'
-
 def unsafe?(speed)
-if speed < 40 || speed > 60
-  return true
-else return false
-end
+  if speed > 60 || speed < 40
+     true
+  elsif speed.between?(40,60)
+     false
+  end
 end
 
+end
 
 def not_safe?(speed)
-speed < 40 || speed > 60 ? true : false
 end
